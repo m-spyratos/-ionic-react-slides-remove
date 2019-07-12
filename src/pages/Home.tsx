ab2905aaca7f23347a4ba8bdc15c07738a444f8a
mspyratos <m.spyratos@gmail.com>
2019-07-12 12:06:56 -0400

    first commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,40 @@
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import {IonContent, IonHeader, IonTitle, IonToolbar, IonSlides, IonSlide, IonButton} from '@ionic/react';
+import React, {useState} from 'react';
+
+const slideOpts = {
+    initialSlide: 0,
+    speed: 400
+};
 
 const Home: React.FunctionComponent = () => {
-  return (
-    <>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Ionic Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-        The world is your oyster.
-        <p>
-          If you get lost, the{' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
-            docs
-          </a>{' '}
-          will be your guide.
-        </p>
-      </IonContent>
-    </>
-  );
+
+    const [slides, setSlides] = useState([
+        {id: '1', text: 'Slide 1'},
+        {id: '2', text: 'Slide 2'},
+        {id: '3', text: 'Slide 3'},
+        {id: '4', text: 'Slide 4'},
+        {id: '5', text: 'Slide 5'}
+    ]);
+
+    return (
+        <>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Ionic Blank</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent>
+
+                <IonButton onClick={() => setSlides(slides.slice(1))}>
+                    Remove first slide
+                </IonButton>
+
+                <IonSlides pager={true} options={slideOpts}>
+                    {slides.map(slide => (<IonSlide key={slide.id}><h1>{slide.text}</h1></IonSlide>))}
+                </IonSlides>
+            </IonContent>
+        </>
+    );
 };
 
 export default Home;
